112afa54af5b691e1ac735b36d8ca0e360c96498
László Balázs <laszloballazs2001@gmail.com>
2025-04-21 14:19:39 +0200

    server config

--- Next_IT/config/db_config.php
+++ Next_IT/config/db_config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'nextit_user');
+define('DB_PASS', 'wKKHv?230QUdt9qAVJ1#');
 define('DB_NAME', 'nextit_db');
 ?>
